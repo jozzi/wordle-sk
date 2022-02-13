33bd104fed38624733b234df408c105d755b6c1d
jozef_radonak <jozef.radonak@hotovo.com>
2022-02-13 15:12:41 +0100

    Make stats historgram nicer

--- src/components/stats/Progress.tsx
+++ src/components/stats/Progress.tsx
@@ -10,8 +10,8 @@ export const Progress = ({ index, size, label }: Props) => {
       <div className="items-center justify-center w-2">{index + 1}</div>
       <div className="rounded-full w-full ml-2">
         <div
-          style={{ width: `${5 + size}%` }}
-          className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-l-full"
+          style={{ width: `${5 + size}%`, backgroundColor: 'rgb(35,237,232)' }}
+          className="text-xs font-medium text-black text-center p-0.5 rounded-full"
         >
           {label}
         </div>
